4c046b613a5940685518270523f2e9fc7ba173a3
Kristin <kristinponsonby@gmail.com>
2021-02-20 12:00:31 -0500

    commit tree 03a77e952ab646e0656fe20075dfe1b7b879e13a

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
 
+
+
+# Checklist
+#{x} Define book class
+#{x} new book instance gets initialized with a title
+#{x} new book instance has a title, author, page count, and genre
+#{x} create a method called turn_page that can turn the page of a book instance
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,35 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand
+        @brand
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+        end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+# Checklist
+# {x} Create a class called shoe
+# {x} A new instance of the shoe class gets initialized with a brand
+# {x} Each instance of the shoe class has a brand, color, size and material
+# {x} Create a method called cobble that puts out the phrae (the shoe has been repaired)
+# {} Method cobble should change the shoe condition to new
